eb7bf4602d6abb6ad1a5a4c6f510912636c16edc
Thomas F. Kent <kentedegrees@gmail.com>
2023-05-01 18:53:37 -0500

    Fix Issue with c function decleration.

--- src/vdra-init.c
+++ src/vdra-init.c
@@ -1,13 +1,14 @@
 #include <R_ext/Rdynload.h>
 #include "compute_cox.h"
 
+void R_init_vdra(DllInfo *dll);
 
 static const R_CallMethodDef Callentries[] = {
   {"ComputeCox",       (DL_FUNC) &ComputeCox,      9},
   {NULL, NULL, 0}
 };
 
-void R_init_vdra(DllInfo *dll){
+void R_init_vdra(DllInfo *dll) {
   R_registerRoutines(dll, NULL, Callentries, NULL, NULL);
   R_useDynamicSymbols(dll, FALSE);
 }
